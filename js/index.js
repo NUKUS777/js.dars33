3220da8f8388f62ff835efd964fabfa2e4fd9f50
Dawlet Kenesbaev <123051703+NUKUS777@users.noreply.github.com>
2023-03-09 14:41:27 +0500

    me

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-const input= document.querySelector('input');
+const input= document.querySelector('.input');
 const result= document.querySelectorAll('.result');
-const btn= document.querySelector('.btn');
+const btn= document.querySelectorAll('.btn');
+const inputCel= document.querySelector('.inputCel');
+const celcTem= document.querySelector('.celcTem');
 const passSpeed = 3.6;
 const velSpeed = 20.1;
 const carSpeed = 70;
@@ -8,10 +10,13 @@ const planeSpeed = 800;
 function  clac(distance,speed) {
     return  (distance/speed).toFixed(2)
 }
-btn.addEventListener("click", ()=>{
+btn[0].addEventListener("click", ()=>{
    result[0].innerHTML = `${clac(input.value,passSpeed)} hours`
    result[1].innerHTML = `${clac(input.value,velSpeed)} hours`
    result[2].innerHTML = `${clac(input.value,carSpeed)} hours`
    result[3].innerHTML = `${clac(input.value,planeSpeed)} hours`
 
+})
+btn[1].addEventListener("click",()=> {
+    celcTem.innerHTML = `${inputCel.value*1.8 +32} F`
 })
